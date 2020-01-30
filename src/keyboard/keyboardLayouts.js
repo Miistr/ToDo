e638e412e1bb5d889403362aa5a8cc190bde441b
Vlad <kapitanvasnakrylo@gmail.com>
2020-01-30 13:51:57 +0300

    feat: added new layout 1. added Caps layout, 2.  rename specials buttons.

--- src/keyboard/keyboardLayouts.js
+++ src/keyboard/keyboardLayouts.js
@@ -1,33 +1,47 @@
 export const enUS = {
   default: [
-    "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-    "{tab} q w e r t y u i o p [ ] \\",
-    "{lock} a s d f g h j k l ; ' {enter}",
-    "{shift} z x c v b n m , . / {shift}",
-    "{ctrl} @ {space}"
+    "` 1 2 3 4 5 6 7 8 9 0 - = Backspace",
+    "Tab q w e r t y u i o p [ ] \\",
+    "CapsLock a s d f g h j k l ; ' Enter",
+    "Shift z x c v b n m , . / Shift",
+    "Control Space @ controll"
   ],
   shift: [
-    "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
-    "{tab} Q W E R T Y U I O P { } |",
-    '{lock} A S D F G H J K L : " {enter}',
-    "{shift} Z X C V B N M < > ? {shift}",
-    "{ctrl} @ {space}"
+    "~ ! @ # $ % ^ & * ( ) _ + Backspace",
+    "Tab Q W E R T Y U I O P { } |",
+    'CapsLock A S D F G H J K L : " Enter',
+    "Shift Z X C V B N M < > ? Shift",
+    "Control Space @ Control"
+  ],
+  caps: [
+    "` 1 2 3 4 5 6 7 8 9 0 - = Backspace",
+    "Tab Q W E R T Y U I O P [ ] \\",
+    "CapsLock A S D F G H J K L ; ' Enter",
+    "Shift Z X C V B N M , . / Shift",
+    "Control Space @ Control"
   ]
 };
 
 export const ruRU = {
   default: [
-    "ё 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-    "{tab} й ц у к е н г ш щ з х ъ \\",
-    "{lock} ф ы в а п р о л д ж э {enter}",
-    "{shift} я ч с м и т ь б ю . {shift}",
-    "{ctrl} @ {space}"
+    "ё 1 2 3 4 5 6 7 8 9 0 - = Backspace",
+    "Tab й ц у к е н г ш щ з х ъ \\",
+    "CapsLock ф ы в а п р о л д ж э Enter",
+    "Shift я ч с м и т ь б ю . Shift",
+    "Control Space @ Control"
   ],
   shift: [
-    'Ё ! " № ; % : ? * ( ) _ + {bksp}',
-    "{tab} Й Ц У К Е Н Г Ш Щ З Х Ъ /",
-    "{lock} Ф Ы В А П Р О Л Д Ж Э {enter}",
-    "{shift} Я Ч С М И Т Ь Б Ю , {shift}",
-    "{ctrl} @ {space}"
+    'Ё ! " № ; % : ? * ( ) _ + Backspace',
+    "Tab Й Ц У К Е Н Г Ш Щ З Х Ъ /",
+    "CapsLock Ф Ы В А П Р О Л Д Ж Э Enter",
+    "Shift Я Ч С М И Т Ь Б Ю , Shift",
+    "Control Space @ Control"
+  ],
+  caps: [
+    "ё 1 2 3 4 5 6 7 8 9 0 - = Backspace",
+    "Tab Й Ц У К Е Н Г Ш Щ З Х Ъ \\",
+    "CapsLock Ф Ы В А П Р О Л Д Ж Э Enter",
+    "Shift Я Ч С М И Т Ь Б Ю . Shift",
+    "Control Space @ Control"
   ]
 };
